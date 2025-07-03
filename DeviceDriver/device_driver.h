606f70f88e9ce939615564cf00186bb9e7451c86
see.jin <bobstar0@naver.com>
2025-07-03 17:10:51 +0900

    devicedriver

--- DeviceDriver/device_driver.h
+++ DeviceDriver/device_driver.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "flash_memory_device.h"
+#include "gmock/gmock.h"
+
+#define TestTryCount (4)
 
 class DeviceDriver
 {
 public:
     DeviceDriver(FlashMemoryDevice* hardware);
-    int read(long address);
-    void write(long address, int data);
+    virtual int read(long address);
+    void postConditionCheck(int ret, long address);
+    void preconditionCheck(long address);
+    virtual void write(long address, int data);
 
 protected:
     FlashMemoryDevice* m_hardware;
+};
+
+
+class MockDeviceDriver : public DeviceDriver {
+public:
+	MockDeviceDriver(FlashMemoryDevice* hardware) : DeviceDriver(hardware) {}
+	MOCK_METHOD(int, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, int data), (override));
 };
